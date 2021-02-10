a9f0a8989c0cf5e5c5a8bf02b010cf29b8c02d68
LAPTOP-P5HU0FF7\ratho <shaktisingh03@gmail.com>
2021-02-10 10:54:45 +0530

    material layout setup

--- resources/frontend2/src/app/app-routing.module.ts
+++ resources/frontend2/src/app/app-routing.module.ts
@@ -1,35 +1,60 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './core/guards/auth.guard';
 
-// const routes: Routes = [
-//   // { path: '', redirectTo: '/home', pathMatch: 'full'},
-//   { path: '', component: HomeComponent },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'signup', component: SignupComponent },
-//   { path: 'dashboard', component: DashboardComponent },
-//   { path: 'settings', component: SettingsComponent },
-//   { path: 'appointment-manager', component: AppointmentManagerComponent },
-//   { path: ':user_url', component: PatientSchedulerComponent },
-//   { path: ':user_url/book', component: PatientScheduler2Component },
-//   { path: ':user_url/scheduleEventConfirm', component: ScheduleEventConfirm },
-
-//   { path: 'appointment-manager/event-types/30-minutes/edit', component: EditAppointmentManagerComponent },
-//   {
-//      path: 'search',
-//      component: SearchComponent
-//   }, {
-//      path: 'new-cmp',
-//      component: NewCmpComponent
-//   },
-//   { path: '**', component: ErrorpageComponent }
-// ])
+const appRoutes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: './auth/auth.module#AuthModule'
+    },
+    {
+        path: 'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'customers',
+        loadChildren: './customers/customers.module#CustomersModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'users',
+        loadChildren: './users/users.module#UsersModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'account',
+        loadChildren: './account/account.module#AccountModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'icons',
+        loadChildren: './icons/icons.module#IconsModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'typography',
+        loadChildren: './typography/typography.module#TypographyModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'about',
+        loadChildren: './about/about.module#AboutModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule],
+    providers: []
 })
 export class AppRoutingModule { }
-
-
