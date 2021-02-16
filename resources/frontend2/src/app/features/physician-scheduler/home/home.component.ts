94c6fc850ee7cb8b95a30ad81c290453b71cc59a
LAPTOP-P5HU0FF7\ratho <shaktisingh03@gmail.com>
2021-02-16 13:01:14 +0530

    appointment page UI

--- resources/frontend2/src/app/features/physician-scheduler/home/home.component.ts
+++ resources/frontend2/src/app/features/physician-scheduler/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, FormGroupDirective } from '@angular/forms';
+
+enum Step {
+  step1,
+  step2
+};
 
 @Component({
   selector: 'physician-scheduler-home',
@@ -6,16 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  Step = Step;
   showTimeSlots: boolean = false;
+  detailForm: FormGroup = new FormGroup({
+    title: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required)
+  })
+  activeStep: Step = Step.step1
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementsByTagName('body')[0].classList.add('bg-default');
     console.log('here loaded');
   }
 
   onSelect(event: any) {
     this.showTimeSlots = true;
+  }
+
+  gotoStep(step: Step) {
+    this.activeStep = step;
   }
 
 }
