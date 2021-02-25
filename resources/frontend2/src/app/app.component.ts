4e435810516c701b283d36529860b4613f20bd43
LAPTOP-P5HU0FF7\ratho <shaktisingh03@gmail.com>
2021-02-25 07:15:26 +0530

    Show loader for lazy loading module

--- resources/frontend2/src/app/app.component.ts
+++ resources/frontend2/src/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
+import { UiService } from './core/services/ui.service';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private ui: UiService) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof RouteConfigLoadStart) {
+        this.ui.showSpinner();
+      } else if (event instanceof RouteConfigLoadEnd) {
+        this.ui.stopSpinner();
+      }
+    });
+  }
+}
