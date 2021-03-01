80a230e7fc2da64d42194302eeb5b32e02e1702e
LAPTOP-P5HU0FF7\ratho <shaktisingh03@gmail.com>
2021-03-01 22:57:31 +0530

    Environment file

--- resources/frontend2/src/environments/environment.prod.ts
+++ resources/frontend2/src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ import { NgxLoggerLevel } from 'ngx-logger';
 
 export const environment = {
   production: true,
+  app_name: 'TeleMedicine',
   logLevel: NgxLoggerLevel.OFF,
-  serverLogLevel: NgxLoggerLevel.ERROR
+  serverLogLevel: NgxLoggerLevel.ERROR,
+  api_url: `${(window as any).baseUrl}/api`
 };
